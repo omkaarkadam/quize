365d7519dd01993a887bd00720c257f37e3a3faa
Omkar Kadam <159976526+omkaarkadam@users.noreply.github.com>
2024-07-07 14:07:32 +0530

    Update questions.js
    write wrong code

--- questions.js
+++ questions.js
@@ -1,137 +1,102 @@
-let questions = [
+const questions = [
     {
-        numb: 1,
         question: "What does HTML stand for?",
-        answer: "Hyper Text Markup Language",
-        options: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Language",
-            "Hyper Text Multiple Language",
-            "Hyper Tool Multi Language"
-        ]
+        options: ["Hyper Text Markup Language", "Hot Mail", "How to Make Lasagna", "Hyperlink and Text Markup Language"],
+        answer: "Hyper Text Markup Language"
     },
     {
-        numb: 2,
         question: "What does CSS stand for?",
-        answer: "Cascading Style Sheet",
-        options: [
-            "Common Style Sheet",
-            "Colorful Style Sheet",
-            "Computer Style Sheet",
-            "Cascading Style Sheet"
-        ]
+        options: ["Computer Style Sheets", "Cascading Style Sheets", "Creative Style Sheets", "Colorful Style Sheets"],
+        answer: "Cascading Style Sheets"
     },
     {
-        numb: 3,
         question: "What does PHP stand for?",
-        answer: "Hypertext Preprocessor",
-        options: [
-            "Hypertext Preprocessor",
-            "Hypertext Programming",
-            "Hypertext Preprogramming",
-            "Hometext Preprocessor"
-        ]
+        options: ["Hypertext Preprocessor", "Preprocessor Home Page", "Pretext Hypertext Processor", "Personal Home Page"],
+        answer: "Hypertext Preprocessor"
     },
     {
-        numb: 4,
-        question: "What does SQL stand for?",
-        answer: "Structured Query Language",
-        options: [
-            "Stylish Question Language",
-            "Stylesheet Query Language",
-            "Statement Question Language",
-            "Structured Query Language"
-        ]
+        question: "What does JS stand for?",
+        options: ["JavaSource", "JavaScript", "JustScript", "JQueryScript"],
+        answer: "JavaScript"
     },
     {
-        numb: 5,
-        question: "What does XML stand for?",
-        answer: "eXtensible Markup Language",
-        options: [
-            "eXtensible Markup Language",
-            "eXecutable Multiple Language",
-            "eXTra Multi-Program Language",
-            "eXamine Multiple Language"
-        ]
-    },
-   
-    {
-        numb: 6,
-        question: "Which property is used to change the background color in CSS?",
-        answer: "background-color",
-        options: [
-            "bgcolor",
-            "background-color",
-            "color",
-            "background"
-        ]
-    },
-  
-    {
-        numb: 7,
-        question: "How do you select an element with the id 'demo' in CSS?",
-        answer: "#demo",
-        options: [
-            ".demo",
-            "#demo",
-            "demo",
-            "*demo"
-        ]
+        question: "What does Java stand for?",
+        options: ["Just Another Virtual Accelerator", "JavaScript", "JavaBeans", "None of the above"],
+        answer: "None of the above"
     },
     {
-        numb: 8,
-        question: "What does the 'var' keyword do in JavaScript?",
-        answer: "Declares a variable",
-        options: [
-            "Declares a function",
-            "Declares a constant",
-            "Declares a variable",
-            "Declares a loop"
-        ]
+        question: "Which tag is used to create a hyperlink in HTML?",
+        options: ["<link>", "<a>", "<href>", "<hyperlink>"],
+        answer: "<a>"
     },
     {
-        numb: 9,
-        question: "Which SQL statement is used to extract data from a database?",
-        answer: "SELECT",
-        options: [
-            "GET",
-            "SELECT",
-            "EXTRACT",
-            "OPEN"
-        ]
+        question: "Which attribute is used in HTML to define inline styles?",
+        options: ["class", "style", "styles", "font"],
+        answer: "style"
     },
     {
-        numb: 10,
-        question: "Which HTML attribute is used to define inline styles?",
-        answer: "style",
-        options: [
-            "styles",
-            "class",
-            "style",
-            "font"
-        ]
-    },
-  
-    {
-        numb: 11,
-        question: "Which event occurs when the user clicks on an HTML element in JavaScript?",
-        answer: "onclick",
-        options: [
-            "onchange",
-            "onmouseclick",
-            "onmouseover",
-            "onclick"
-        ]
+        question: "Which PHP function is used to connect to a MySQL database?",
+        options: ["mysql_connect()", "mysql_open()", "mysql_query()", "mysql_db()"],
+        answer: "mysql_connect()"
     },
     {
-        numb: 12,
-        question: "What is the correct SQL statement to create a new table?",
-        answer: "CREATE TABLE",
-        options: [
-            "ADD TABLE",
-            "CREATE TABLE",
-            "NEW TABLE",
-            "MAKE TABLE"
-        ]
+        question: "Which MySQL command is used to create a new database?",
+        options: ["CREATE DATABASE", "MAKE DATABASE", "NEW DATABASE", "ADD DATABASE"],
+        answer: "CREATE DATABASE"
     },
+    {
+        question: "Which Bootstrap class is used to create a button?",
+        options: [".btn", ".button", ".btn-default", ".button-default"],
+        answer: ".btn"
+    },
+    {
+        question: "Which Bootstrap class is used to create a responsive table?",
+        options: [".table-responsive", ".table", ".responsive-table", ".table-responsive-table"],
+        answer: ".table-responsive"
+    },
+    {
+        question: "Which JavaScript method is used to write HTML output?",
+        options: ["document.write()", "console.log()", "window.write()", "output.write()"],
+        answer: "document.write()"
+    },
+    {
+        question: "Which JavaScript method is used to select an HTML element by its ID?",
+        options: ["getElementById()", "querySelector()", "getElementByClass()", "getElementByTag()"],
+        answer: "getElementById()"
+    },
+    {
+        question: "Which MySQL command is used to insert data into a table?",
+        options: ["INSERT INTO", "ADD INTO", "INSERT DATA", "ADD DATA"],
+        answer: "INSERT INTO"
+    },
+    {
+        question: "Which PHP function is used to get the length of a string?",
+        options: ["strlen()", "strlength()", "length()", "count()"],
+        answer: "strlen()"
+    },
+    {
+        question: "Which HTML tag is used to create an ordered list?",
+        options: ["<ol>", "<ul>", "<li>", "<list>"],
+        answer: "<ol>"
+    },
+    {
+        question: "Which Bootstrap class is used to add a background color to an element?",
+        options: [".bg-color", ".background-color", ".bg-primary", ".background-primary"],
+        answer: ".bg-primary"
+    },
+    {
+        question: "Which JavaScript method is used to add an element at the end of an array?",
+        options: ["push()", "pop()", "add()", "insert()"],
+        answer: "push()"
+    },
+    {
+        question: "Which MySQL command is used to update data in a table?",
+        options: ["UPDATE", "CHANGE", "MODIFY", "ALTER"],
+        answer: "UPDATE"
+    },
+    {
+        question: "Which PHP superglobal array holds the data submitted via an HTTP POST request?",
+        options: ["$_POST", "$_GET", "$_REQUEST", "$_SESSION"],
+        answer: "$_POST"
+    }
 ];
